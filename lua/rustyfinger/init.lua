f63f5bb6cd429a17e420daaaa7ab43e92347ba57
Mickey Chen <poverwhelming@gmail.com>
2024-03-03 18:35:34 +0800

    setup treesitter & strip trailing whitespace

--- lua/rustyfinger/init.lua
+++ lua/rustyfinger/init.lua
@@ -2,3 +2,19 @@ require("rustyfinger.set")
 require("rustyfinger.remap")
 
 require("rustyfinger.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local NvimGroup = augroup("NvimGroup", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd({"BufWritePre"}, {
+    group = "NvimGroup",
+    pattern = [[*\(.md\)\@<!]],
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
--- lua/rustyfinger/lazy/treesitter.lua
+++ lua/rustyfinger/lazy/treesitter.lua
@@ -1,24 +1,56 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+
     build = ":TSUpdate",
+
     config = function()
         require("nvim-treesitter.configs").setup({
+            -- A list of parser names, or "all" (the five listed parsers should always be installed)
             ensure_installed = {
                 "bash",
                 "css",
                 "elixir",
                 "erlang",
+                "heex",
                 "html",
                 "javascript",
                 "jsdoc",
                 "lua",
+                "markdown_inline",
                 "query",
+                "regex",
                 "scss",
                 "sql",
                 "typescript",
                 "vimdoc",
                 "zig",
-            }
+            },
+
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            indent = { enable = true },
+
+            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+            parser_install_dir = "~/.nvim/treesitter/parsers",
+
+            highlight = {
+                -- `false` will disable treesitter extension
+                enable = true,
+
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = { "markdown" },
+            },
         })
+
+        vim.opt.runtimepath:append("~/.nvim/treesitter/parsers")
     end
 }
+
