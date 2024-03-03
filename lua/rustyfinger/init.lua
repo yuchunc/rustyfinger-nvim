f0078ac0f491fa1fcaa344c3d6fed20577029d57
Mickey Chen <poverwhelming@gmail.com>
2024-03-03 16:03:37 +0800

    initial commit

---
+++ lua/rustyfinger/init.lua
@@ -0,0 +1,4 @@
+require("rustyfinger.set")
+require("rustyfinger.remap")
+
+require("rustyfinger.lazy_init")
---
+++ lua/rustyfinger/set.lua
@@ -0,0 +1,17 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
